1ed8d69939d269b30b38c0398f46e4ea5e97be59
Yeimer Rojas <yeimerrojasb@hotmail.com>
2018-10-18 15:24:01 -0500

    preparacion para el menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './view/login/login.component';
+import { InicioComponent } from './view/inicio/inicio.component';
+import { ChatComponent } from './view/chat/chat.component';
+import { PerfilComponent } from './view/perfil/perfil.component';
+import { from } from 'rxjs';
+import { MenuComponent } from './view/menu/menu.component';
+ const appRutas: Routes = [
+   {path: '', component: InicioComponent},
+   {path: 'inicio', component: InicioComponent},
+   {path: 'chat', component: ChatComponent},
+   {path: 'perfil', component: PerfilComponent},
+   {path: 'login', component: LoginComponent}
+
+ ]; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    ChatComponent,
+    PerfilComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
